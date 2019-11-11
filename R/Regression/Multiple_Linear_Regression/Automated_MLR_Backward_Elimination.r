e8fbf98607a655b1a81b07767dd20873dad2d6d2
luther001 <paulluther02@gmail.com>
2019-11-11 19:28:08 +0530

    Add files via upload

---
+++ R/Regression/Multiple_Linear_Regression/Automated_MLR_Backward_Elimination.r
@@ -0,0 +1,47 @@
+#impoting dataset
+dataset = read.csv('50_Startups.csv')View(dataset)
+# dataset = dataset[, 2:3]
+
+# Encoding categorical data
+dataset$State = factor(dataset$State,
+                       levels = c('New York', 'California', 'Florida'),
+                       labels = c(1, 2, 3))
+
+#Splitting the Dataset into Training set and Test set
+#install.packages('caTools')
+library(caTools)
+set.seed(123)
+split = sample.split(dataset$Profit, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE) 
+
+#Fitting MLR to the Training Set
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State , data = training_set)
+
+#predecting the test set result
+y_pred = predict(regressor, newdata = test_set)
+
+
+
+
+
+
+
+
+backwardElimination <- function(x, sl) {
+  numVars = length(x)
+  for (i in c(1:numVars)){
+    regressor = lm(formula = Profit ~ ., data = x)
+    maxVar = max(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"])
+    if (maxVar > sl){
+      j = which(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"] == maxVar)
+      x = x[, -j]
+    }
+    numVars = numVars - 1
+  }
+  return(summary(regressor))
+}
+
+SL = 0.05
+dataset = dataset[, c(1,2,3,4,5)]
+backwardElimination(training_set, SL)
---
+++ R/Regression/Multiple_Linear_Regression/multiple_linear_regression.r
@@ -0,0 +1,2 @@
+#impoting dataset
+dataset = read.csv('50_Startups.csv')
