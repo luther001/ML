5b3569466274219a5ef5de28c6b46ac72b22e0af
Paul Wesley Luther <paulluther02@gmail.com>
2019-11-13 09:24:01 +0530

    Update polynomial_regression.r

--- R/Regression/Polynomial_Regression/polynomial_regression.r
+++ R/Regression/Polynomial_Regression/polynomial_regression.r
@@ -8,7 +8,7 @@ dataset$Level3 = dataset$Level^3
 dataset$Level4 = dataset$Level^4
 poly_reg = lm(formula = Salary ~ .,data = dataset)
 
-#visualizing the dataset
+#visualizing the Polynomial Regression Result
 ggplot() + 
   geom_point(aes(x = dataset$Level, y = dataset$Salary), colour = 'red') + 
   geom_line(aes(x = dataset$Level, y = predict(poly_reg, newdata = dataset)), colour = 'blue') +
@@ -16,4 +16,5 @@ ggplot() +
   xlab('Level') +
   ylab('Salary')
 
+#Predicting new result with Polynomial Regression 
 y_pred = predict(poly_reg, data.frame(Level = 6.5, Level2 = 6.5^2, Level3 = 6.5^3, Level4 = 6.5^4))
